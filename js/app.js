ab7f74eb67fceca6752f1a9fd8b43e45c12f2f50
Trevin Pick <trevinp@gmail.com>
2018-08-31 22:48:11 -0700

    Major updates

--- js/app.js
+++ js/app.js
@@ -1,11 +1,6 @@
 /*
-test
  * Create a list that holds all of your cards
  */
-
- //let cardDeck = document.getElementById("cardDeck");
- //cardDeck.style.display = "auto";
- //cardDeck.addEventListener("click",function(event) {cardDeck.style.display = "none";});
 
 /*
  * Display the cards on the page
@@ -41,34 +36,74 @@ function shuffle(array) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
- // The master list of cards the user can play with on the board
-let cardList = ['fa-diamond', 'fa-diamond', 'fa-paper-plane-o', 'fa-paper-plane-o',
-                'fa-anchor','fa-anchor', 'fa-bolt','fa-bolt', 'fa-cube', 'fa-cube',
-                'fa-leaf', 'fa-leaf', 'fa-bicycle', 'fa-bicycle', 'fa-bomb', 'fa-bomb'];
+// The master list of cards the user can play with on the board
+let initialCards = ['fa-diamond', 'fa-diamond', 'fa-paper-plane-o', 'fa-paper-plane-o',
+    'fa-anchor', 'fa-anchor', 'fa-bolt', 'fa-bolt', 'fa-cube', 'fa-cube',
+    'fa-leaf', 'fa-leaf', 'fa-bicycle', 'fa-bicycle', 'fa-bomb', 'fa-bomb'];
 
 // List to hold which cards have been selected by user
 let selectedCards = [];
+let numMoves = 0;
+let numMovesElement = document.getElementById('moveCounter');
 
-buildCards(cardList);
-addClickEventToCards();
+initalizeCardGame();
+
+function initalizeCardGame() {
+    buildCards(shuffle(initialCards));
+    addClickEventToCards();
+    addRestartEvent();
+    numMovesElement.innerText = numMoves;
+}
+
+function addRestartEvent() {
+    // Restart funcitonality- remove cards and reinitialize
+    let restart = document.getElementById('restart');
+    restart.addEventListener('click', function () {
+        selectedCards = [];
+        numMoves = 0;
+        let deck = document.getElementById('cardDeck');
+        let children = deck.querySelectorAll('.card');
+        for (const child of children) {
+            deck.removeChild(child);
+        }
+        buildCards(shuffle(initialCards));
+        addClickEventToCards();
+    });
+}
 
 function addClickEventToCards() {
     // Adds click event to cards to either show or flip the card back over
     let cards = document.querySelectorAll('.card');
 
     cards.forEach(function (card) {
-        card.addEventListener('click', function (ev) {
+        card.addEventListener('click', function () {
             if (!card.classList.contains('open') && !card.classList.contains('match')) {
                 selectedCards.push(card);
                 card.classList.add('open', 'show');
-                if (selectedCards.length == 2) {
-                    setTimeout(function () {
-                        resetCards();
-                    }, 1000);
-                }
+                checkForMatch();
             }
         });
     });
+}
+
+function checkForMatch() {
+    if (selectedCards.length == 2) {
+        // Check if cards match by checking class name of 1st and 2nd items
+        let classCardName1 = selectedCards[0].firstElementChild.classList[1];
+        let classCardName2 = selectedCards[1].firstElementChild.classList[1];
+        if (classCardName1 === classCardName2) {
+            // It matches
+            cardsMatch();
+        }
+        else {
+            // It does not match
+            setTimeout(function () {
+                resetCards();
+            }, 1000);
+            numMoves = numMoves + 1;
+            numMovesElement.innerText = numMoves;
+        }
+    }
 }
 
 function buildCards(cardList) {
@@ -94,4 +129,10 @@ function resetCards() {
     }
     selectedCards = [];
 }
- 
+
+function cardsMatch() {
+    for (const card of selectedCards) {
+        card.classList.add('match');
+    }
+    selectedCards = [];
+}
